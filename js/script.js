24b33395bd1f20b1a5d1b8b4ed470fbd628d22a0
kailanyb <80835996+kailanyb@users.noreply.github.com>
2021-04-11 22:14:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const span = document.querySelector('span')
+const navegacaoconteudo = document.querySelector('.navegacaoconteudo')
+/*const menul = document.querySelector('.menul')
+const menur = document.querySelector('.menur')
+const menu = document.querySelector('.menu')
+const fundomenu = document.querySelector('.fundomenu')*/
 
+
+span.addEventListener('click', function() {
+  navegacaoconteudo.style.display = 'flex'
+})
+    /*if (menupc.style.display !== 'flex') {
+        menupc.style.display = 'flex'
+        menul.style.display = 'flex'
+        menur.style.display = 'flex'
+        menu.style.height = '230px'
+        fundomenu.style.display = 'block'
+    } else {
+        menupc.style.display = ''
+        menul.style.display = ''
+        menur.style.display = ''
+        menu.style.height = ''
+        fundomenu.style.display = ''
+    }
+})*/
