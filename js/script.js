0d3624963c522658546f8768a832d0ccff012d95
kailanyb <80835996+kailanyb@users.noreply.github.com>
2021-04-12 00:26:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const span = document.querySelector('span')
 const menu = document.querySelector('.menu')
 const menul = document.querySelector('.menul')
-/*const menur = document.querySelector('.menur')
-const menu = document.querySelector('.menu')
+const menulogo = document.querySelector('.menulogo')
+/*const menu = document.querySelector('.menu')
 const fundomenu = document.querySelector('.fundomenu')*/
 
 
@@ -10,9 +10,11 @@ span.addEventListener('click', function() {
   if(menu.style.display !== 'flex') {
     menu.style.display = 'flex'
     menul.style.display = 'flex'
+    menulogo.style.display = 'flex'
   } else {
     menu.style.display = ''
     menul.style.display = ''
+    menulogo.style.display = ''
   }
 })
     /*if (menupc.style.display !== 'flex') {
